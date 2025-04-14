52668cb3ef83d718efafe0365e6eb09fa27a1587
Jake Goldfarb <jgoldfarb@composecure.com>
2025-04-14 10:06:38 -0400

    Remove boiler plate, add WIP placeholders, fonts, readme, meta

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-oswald)', 'sans-serif'],
+        heading: ['var(--font-bebas-neue)', 'sans-serif'],
+      },
+    },
+  },
+}
