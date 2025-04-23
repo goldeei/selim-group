94173173a92f89f5b091210302df2ba25231d7f7
Jake Goldfarb <jgoldfarb@composecure.com>
2025-04-23 14:02:20 -0400

    Add content

--- src/components/sections/LandingPage.tsx
+++ src/components/sections/LandingPage.tsx
@@ -1,9 +1,21 @@
+import LandingPageFooter from "@/app/assets/svgs/landing-page-footer.svg";
 import LandingPageLogo from "@/app/assets/svgs/landing-page__desktop.svg";
+import { Separator } from "../ui/Separator";
 
 export const LandingPage = () => {
 	return (
-		<div className="bg-primary">
-			<LandingPageLogo />
+		<div className="bg-primary w-screen h-screen flex flex-col justify-center items-center overflow-hidden">
+			<div className="w-full max-w-4xl flex flex-col items-center gap-4 mt-auto">
+				<LandingPageLogo
+					className="w-4/5 h-full"
+					preserveAspectRatio="xMidYMid meet"
+				/>
+				<Separator />
+				<h2 className="text-primary__lighter tracking-widest">
+					WE DO HOUSES AND STUFF
+				</h2>
+			</div>
+			<LandingPageFooter className="mt-auto" />
 		</div>
 	);
 };
