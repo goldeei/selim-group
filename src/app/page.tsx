77ad0122cba8e0b8ecf2c1568db3f86ca4a57479
Jake Goldfarb <jgoldfarb@composecure.com>
2025-04-23 13:04:45 -0400

    Add landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import { LandingPage } from "@/components/sections/LandingPage";
+
 export default function Home() {
 	return (
-		<div className="flex flex-col size-full items-center justify-center gap-2">
-			<h1 className="text-5xl tracking-wide text-shadow ">The Selim Group</h1>
-			<h2 className="text-2xl text-shadow font-sans tracking-widest">WIP</h2>
+		<div>
+			<LandingPage />
 		</div>
 	);
 }
