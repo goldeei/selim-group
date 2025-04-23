1812102406984f9bd793b9e6c5252ab3f1c4b236
Jake Goldfarb <jgoldfarb@composecure.com>
2025-04-23 13:12:00 -0400

    Config svgr

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			issuer: /\.[jt]sx?$/,
+			use: [
+				{
+					loader: "@svgr/webpack",
+					options: {
+						typescript: true,
+						dimensions: false,
+					},
+				},
+			],
+		});
+
+		return config;
+	},
 };
 
 export default nextConfig;
