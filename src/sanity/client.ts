a25f62b10279336d19cc137d7becb1afafdeae47
Jake Goldfarb <jgoldfarb@composecure.com>
2025-04-24 11:57:01 -0400

    Remove test don't reject unauthed

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "next-sanity";
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 export const client = createClient({
 	projectId: "yjp7gm6e",
 	dataset: "production",
