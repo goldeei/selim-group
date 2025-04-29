ef245b95795e80078e71dd2616aae469cb010dbd
Jake Goldfarb <jgoldfarb@composecure.com>
2025-04-29 09:54:46 -0400

    Add aria label

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -3,7 +3,10 @@ import { NavMenu } from "./NavMenu";
 
 export const NavBar = () => {
 	return (
-		<nav className="sticky top-0  h-fit bg-background p-2">
+		<nav
+			aria-label="Main nav"
+			className="sticky top-0  h-fit bg-background p-2"
+		>
 			<div className="max-content-width mx-auto flex justify-between">
 				<Brand className="w-20 h-fit" />
 				<NavMenu />
