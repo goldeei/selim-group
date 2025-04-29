f17caf51f9e3255c32cf3f9ac61d345721db81c6
Jake Goldfarb <jgoldfarb@composecure.com>
2025-04-29 08:48:06 -0400

    Add brand content

---
+++ src/components/Brand.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import BrandLogo from "@/app/assets/svgs/brand.svg";
+import { cn } from "@/lib/utils";
+import { SVGProps } from "react";
+
+export const Brand = (props: SVGProps<SVGSVGElement>) => {
+	const { className, ...rest } = props;
+
+	const handleClick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+
+	return (
+		<button
+			title="Brand"
+			className="cursor-pointer"
+			onPointerDown={handleClick}
+		>
+			<BrandLogo
+				{...rest}
+				className={cn("hover:brightness-110 transition-all", className)}
+			/>
+		</button>
+	);
+};
