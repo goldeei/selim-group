ef7098135c2b478c9b66b26aea2018ca6f156972
Jake Goldfarb <jgoldfarb@composecure.com>
2025-04-29 08:48:34 -0400

    Layout and add brand

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,7 +1,11 @@
+import { Brand } from "@/components/Brand";
+
 export const NavBar = () => {
 	return (
-		<nav className="sticky top-0  h-fit bg-background">
-			<div className="max-content-width mx-auto">Nav HELLO</div>
+		<nav className="sticky top-0  h-fit bg-background p-2">
+			<div className="max-content-width mx-auto flex justify-between">
+				<Brand className="w-20 h-fit" />
+			</div>
 		</nav>
 	);
 };
