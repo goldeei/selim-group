1849e1c4123c0dbdf7e72d34e68099d574ca7424
Jake Goldfarb <jgoldfarb@composecure.com>
2025-04-29 09:39:53 -0400

    Add navmenu

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,10 +1,12 @@
 import { Brand } from "@/components/Brand";
+import { NavMenu } from "./NavMenu";
 
 export const NavBar = () => {
 	return (
 		<nav className="sticky top-0  h-fit bg-background p-2">
 			<div className="max-content-width mx-auto flex justify-between">
 				<Brand className="w-20 h-fit" />
+				<NavMenu />
 			</div>
 		</nav>
 	);
