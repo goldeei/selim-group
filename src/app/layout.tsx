60c5c9845ca9ea1654d4dd002a7123ff06686c4b
Jake Goldfarb <jake.goldiee@gmail.com>
2025-05-10 14:18:39 -0400

    Include smooth scrolling class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" className="size-full">
+		<html lang="en" className="size-full scroll-smooth">
 			<head>
 				<meta
 					name="viewport"
