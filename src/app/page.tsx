83b8da7bd813ace66f69b6e50c8c1b0c7eeaf679
Jake Goldfarb <jake.goldiee@gmail.com>
2025-05-10 09:36:36 -0400

    Add services section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { LandingPage } from "@/components/sections/landing-page/LandingPage";
+import { Services } from "@/components/sections/services/Services";
 
 export default function Home() {
 	return (
 		<div>
 			<LandingPage />
+			<Services />
 		</div>
 	);
 }
