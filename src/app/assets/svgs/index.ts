b2177169002d0d6f3248b3c0087df33cdb69bcbb
Jake Goldfarb <jake.goldiee@gmail.com>
2025-05-12 18:13:24 -0400

    Refactor SVG assets: update dumpster.svg for responsive design, create index.ts for SVG imports, and adjust styles in other SVG files for consistency.

---
+++ src/app/assets/svgs/index.ts
@@ -0,0 +1,19 @@
+import Brand from "./brand.svg";
+import Dumpster from "./dumpster.svg";
+import InteriorDemolition from "./interior-demolition.svg";
+import InteriorRenovation from "./interior-renovation.svg";
+import LandingPageFooter from "./landing-page-footer.svg";
+import LandingPageMobile from "./landing-page__mobile.svg";
+import LicensedRealtors from "./licensed-realtors.svg";
+import RealEstateInvestment from "./real-estate-investment.svg";
+
+export {
+	Brand,
+	Dumpster,
+	InteriorDemolition,
+	InteriorRenovation,
+	LandingPageFooter,
+	LandingPageMobile,
+	LicensedRealtors,
+	RealEstateInvestment,
+};
