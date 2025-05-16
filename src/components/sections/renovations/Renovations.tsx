604b5c2d30f387fb6fe3137f54bad878e56a8728
Jake Goldfarb <jgoldfarb@composecure.com>
2025-05-16 13:55:12 -0400

    Add Renovations component with section layout

---
+++ src/components/sections/renovations/Renovations.tsx
@@ -0,0 +1,7 @@
+export const Renovations = () => {
+	return (
+		<section id="renovations" className="bg-grey__darker">
+			<h2 className="responsive mb-4 text-grey__lightest">Renovations</h2>
+		</section>
+	);
+};
