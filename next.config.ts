c4ef0cb4ffd130fdbf7c70082b2656cf28f4f9d1
Jake Goldfarb <jake.goldiee@gmail.com>
2025-05-17 10:46:39 -0400

    Enhance Next.js configuration to support SVG imports with additional options for icon handling and optimization

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,34 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
-	webpack(config: NextConfig) {
+const nextConfig: NextConfig = {
+	reactStrictMode: true,
+
+	webpack(config) {
 		config.module.rules.push({
 			test: /\.svg$/,
 			use: ["@svgr/webpack"],
 		});
+
+		config.module.rules.push({
+			test: /\.svg\?react$/i,
+			use: [
+				{
+					loader: "@svgr/webpack",
+					options: {
+						icon: true,
+						svgoConfig: {
+							plugins: [
+								{
+									name: "preset-default",
+									params: { overrides: { removeViewBox: true } },
+								},
+							],
+						},
+					},
+				},
+			],
+		});
+
 		return config;
 	},
 };
