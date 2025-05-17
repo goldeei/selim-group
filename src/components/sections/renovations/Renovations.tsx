96c50e951b4ea1486b5eb36f6e8c7303817293aa
Jake Goldfarb <jake.goldiee@gmail.com>
2025-05-17 11:43:39 -0400

    Update Renovations section to occupy full screen height

--- src/components/sections/renovations/Renovations.tsx
+++ src/components/sections/renovations/Renovations.tsx
@@ -2,7 +2,7 @@ import { PropertyCarousel } from "@/components/property-carousel/PropertyCarouse
 
 export const Renovations = () => {
 	return (
-		<section id="renovations" className="bg-grey__darker">
+		<section id="renovations" className="bg-grey__darker h-screen">
 			<h2 className="responsive mb-4 text-grey__lightest">Renovations</h2>
 			<PropertyCarousel />
 		</section>
