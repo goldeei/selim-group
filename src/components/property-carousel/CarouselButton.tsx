ce480a33abe4db235f9f7a74cb7f5643adaf62db
Jake Goldfarb <jake.goldiee@gmail.com>
2025-05-29 19:35:08 -0400

    Remove unnecessary absolute positioning from carousel button styles

--- src/components/property-carousel/CarouselButton.tsx
+++ src/components/property-carousel/CarouselButton.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 import { CarouselNext, CarouselPrevious } from "../ui/carousel";
 
 export const carouselButtonClasses =
-	"absolute z-20 bg-primary__lighter text-grey__darker border-primary__light border-1 hover:bg-primary__light";
+	"z-20 bg-primary__lighter text-grey__darker border-primary__light border-1 hover:bg-primary__light";
 
 interface CarouselButtonProps {
 	dir: "prev" | "next";
