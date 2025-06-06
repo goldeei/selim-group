5187f06b4a31c5faff51e44a0f1b7abfaba4c0bc
Jake Goldfarb <jgoldfarb@composecure.com>
2025-06-06 10:53:21 -0400

    Update schemaTypes to include the new property schema

--- studio-selim-group/schemaTypes/index.ts
+++ studio-selim-group/schemaTypes/index.ts
@@ -1,4 +1,5 @@
 import {landingPage} from './landingPage'
+import {property} from './property'
 import {service} from './service'
 
-export const schemaTypes = [landingPage, service]
+export const schemaTypes = [landingPage, service, property]
