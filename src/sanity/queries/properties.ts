9d4e1abcfb5acf6629b7ea8d0200429f7f9d0b56
Jake Goldfarb <jgoldfarb@composecure.com>
2025-06-06 13:25:06 -0400

    Get image full image object

--- src/sanity/queries/properties.ts
+++ src/sanity/queries/properties.ts
@@ -2,16 +2,24 @@ import { defineQuery } from "next-sanity";
 
 import { SanityTypes } from "./index";
 
-type PropertyQueryResult = Pick<
-	SanityTypes.Property,
-	"_id" | "title" | "description" | "altText"
-> & {
-	imageUrl: string;
-	imageMetadata: {
-		dimensions: {
-			width: number;
-			height: number;
-		};
+type PropertyQueryResult = {
+	_id: string;
+	title: string | null;
+	description: string | null;
+	altText: string | null;
+	image: {
+		asset: {
+			_id: string;
+			url: string;
+			metadata: {
+				dimensions: {
+					width: number;
+					height: number;
+				} | null;
+			} | null;
+		} | null;
+		hotspot: SanityTypes.SanityImageHotspot | null;
+		crop: SanityTypes.SanityImageCrop | null;
 	} | null;
 };
 
@@ -21,8 +29,34 @@ const PROPERTIES_QUERY = defineQuery(`
     title,
     description,
     altText,
-    "imageUrl": image.asset->url,
-    "imageMetadata": image.asset->metadata
+    image {
+      _type,
+      asset-> {
+        _id,
+        _type,
+        url,
+        metadata {
+          dimensions {
+            width,
+            height
+          },
+          lqip,
+          blurHash
+        }
+      },
+      hotspot {
+        x,
+        y,
+        height,
+        width
+      },
+      crop {
+        top,
+        bottom,
+        left,
+        right
+      }
+    }
   }
 `);
 
