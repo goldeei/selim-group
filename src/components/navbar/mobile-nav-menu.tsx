2dfaa017d894cc981c7494cde5dd3b6de08a1289
Jake Goldfarb <jgoldfarb@composecure.com>
2025-06-10 08:05:47 -0400

    Add missed "use client"

--- src/components/navbar/mobile-nav-menu.tsx
+++ src/components/navbar/mobile-nav-menu.tsx
@@ -1,8 +1,7 @@
+"use client";
+
 import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuTrigger,
+    DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
 import { cn } from "@/lib/utils";
 import { MenuIcon } from "lucide-react";
