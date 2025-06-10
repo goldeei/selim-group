aad043156c9708acf05534bd35b271bf75df4921
Jake Goldfarb <jgoldfarb@composecure.com>
2025-06-10 08:53:31 -0400

    Barrel export

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import { LandingPage } from "@/components/sections/landing-page";
-import { Renovations } from "@/components/sections/renovations";
-import { Services } from "@/components/sections/services";
+import { LandingPage, Renovations, Services } from "@/components/sections";
 
 export default function Home() {
 	return (
---
+++ src/components/sections/index.ts
@@ -0,0 +1,3 @@
+export * from "./landing-page";
+export * from "./renovations";
+export * from "./services";
