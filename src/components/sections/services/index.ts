b9b9d4cc88926d22194db2fd9e24e23a1765d90c
Jake Goldfarb <jgoldfarb@composecure.com>
2025-06-10 08:29:33 -0400

    Add export

--- src/components/sections/services/index.ts
+++ src/components/sections/services/index.ts
@@ -1,6 +1,6 @@
 export * from "./service-description";
 export * from "./service-description-container";
-export * from "./service-item";
 export * from "./service-toggle";
 export * from "./services";
+export * from "./services-container";
 export type { ServiceItem } from "./types";
