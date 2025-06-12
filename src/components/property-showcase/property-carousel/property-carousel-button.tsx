dc486e61b44f9d21a0bb3404b3ef9d32e7cc8800
Jake Goldfarb <jgoldfarb@composecure.com>
2025-06-12 10:19:03 -0400

    Increase size

--- src/components/property-showcase/property-carousel/property-carousel-button.tsx
+++ src/components/property-showcase/property-carousel/property-carousel-button.tsx
@@ -1,13 +1,13 @@
+import { CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import { cn } from "@/lib/utils";
 import { motion } from "motion/react";
 
-import { CarouselNext, CarouselPrevious } from "../../ui/carousel";
 import { controlStyles, controlVariants } from "./styles";
 
 export const styles = {
 	wrapper: "flex items-center rounded-full",
 	button: cn(
-		"!static !transform-none !left-auto !right-auto !top-auto !translate-x-0 !translate-y-0 [&_svg]:!size-[14px] size-4 bg-transparent hover:bg-transparent",
+		"!static !transform-none !left-auto !right-auto !top-auto !translate-x-0 !translate-y-0 [&_svg]:!size-3 size-5 bg-transparent hover:bg-transparent",
 		`text-${controlStyles.color.hover} hover:text-grey__dark`,
 		controlStyles.container
 	),
