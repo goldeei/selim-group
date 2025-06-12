c029c3c28ff8fb9c5792f11e5142e36b95169687
Jake Goldfarb <jgoldfarb@composecure.com>
2025-06-12 08:06:03 -0400

    Update CarouselButton styles for improved appearance

--- src/components/property-showcase/property-carousel/property-carousel-button.tsx
+++ src/components/property-showcase/property-carousel/property-carousel-button.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 import { CarouselNext, CarouselPrevious } from "../../ui/carousel";
 
 export const styles =
-	"z-20 bg-primary__lighter text-grey__darker border-primary__light border-1 hover:bg-primary__light" as const;
+	"w-6 h-1/3 bg-grey text-grey__darker border-grey__darker border-1 hover:bg-grey__light" as const;
 
 interface CarouselButtonProps {
 	dir: "prev" | "next";
