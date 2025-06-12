d37daf00f27d6a72aedd100536cc5666d08da4d8
Jake Goldfarb <jgoldfarb@composecure.com>
2025-06-12 08:05:56 -0400

    Remove unused isDescriptionOpen prop from PropertyCarousel component

--- src/components/property-showcase/property-showcase.tsx
+++ src/components/property-showcase/property-showcase.tsx
@@ -43,7 +43,6 @@ export const PropertyShowcase = ({
 			<PropertyCarousel
 				properties={properties}
 				isMobile={isMobile}
-				isDescriptionOpen={isDescriptionOpen}
 				onPropertyChange={setActiveProperty}
 			/>
 		</div>
