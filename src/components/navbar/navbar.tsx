66afa153e730654fe1cb2dfbc8442ee931a0a822
Jake Goldfarb <jgoldfarb@composecure.com>
2025-06-18 15:19:19 -0400

    raise z-index to 40

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -11,7 +11,7 @@ import { NavMenuContainer } from "./nav-menu-container";
 import { NavMenuItemName } from "./types";
 
 const styles = {
-	container: `h-[${LAYOUT.NAVBAR_HEIGHT}] w-full fixed z-10 top-0 bg-background gutter`,
+	container: `h-[${LAYOUT.NAVBAR_HEIGHT}] w-full fixed z-40 top-0 bg-background gutter`,
 	content: "h-full max-content-width mx-auto flex justify-between items-center",
 } as const;
 
