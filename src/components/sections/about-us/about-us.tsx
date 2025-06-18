7c17da653e2852de7a9c91cf0f72bdf8d6493a68
Jake Goldfarb <jgoldfarb@composecure.com>
2025-06-18 15:18:11 -0400

    All about-us to stretch to content

--- src/components/sections/about-us/about-us.tsx
+++ src/components/sections/about-us/about-us.tsx
@@ -5,7 +5,7 @@ import { getPageByType } from "@/sanity/queries";
 import { AboutUsPage } from "@/types/pages";
 
 const styles = {
-	container: "h-screen min-h-fit flex flex-col bg-grey__lightest",
+	container: "flex flex-col bg-grey__lightest",
 	content: "flex flex-col gap-2",
 } as const;
 
