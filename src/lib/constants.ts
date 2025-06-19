987d8f2b3b17db5b153338738ab236b61e8f853c
Jake Goldfarb <jgoldfarb@composecure.com>
2025-06-19 14:30:48 -0400

    Add global config for main page sections

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,3 +2,27 @@ export const LAYOUT = {
 	NAVBAR_HEIGHT: "4rem",
 	NAVBAR_HEIGHT_PX: 80,
 } as const;
+
+type MainPageSections = "services" | "renovations" | "aboutUs";
+type MainPageSection = {
+	id: string;
+	title: string;
+	href: string;
+};
+export const MAIN_PAGE_SECTIONS: Record<MainPageSections, MainPageSection> = {
+	services: {
+		id: "services",
+		title: "Services",
+		href: "#services",
+	},
+	renovations: {
+		id: "renovations",
+		title: "Renovations",
+		href: "#renovations",
+	},
+	aboutUs: {
+		id: "about-us",
+		title: "About Us",
+		href: "#about-us",
+	},
+} as const;
